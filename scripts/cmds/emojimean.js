5485cbc0579f21b59f0b5fd25b082f385b183231
opusense00 <opusense00@gmail.com>
2025-11-03 01:35:50 +0600

    Update emojimean.js

--- scripts/cmds/emojimean.js
+++ scripts/cmds/emojimean.js
@@ -23,7 +23,7 @@ module.exports = {
 			vi: "Tìm nghĩa của emoji",
 			en: "Find the meaning of emoji"
 		},
-		category: "wiki",
+		category: "info",
 		guide: {
 			vi: "   {pn} <emoji>: Tìm nghĩa của emoji",
 			en: "   {pn} <emoji>: Find the meaning of emoji"
