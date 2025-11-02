3ab37e530d9a91e3b8d96c1054d113098ae89b18
opusense00 <opusense00@gmail.com>
2025-11-03 01:45:04 +0600

    Update appstore.js

--- scripts/cmds/appstore.js
+++ scripts/cmds/appstore.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Tìm app trên appstore",
 			en: "Search app on appstore"
 		},
-		category: "software",
+		category: "utility",
 		guide: "   {pn}: <keyword>"
 			+ "\n   - Example:"
 			+ "\n   {pn} PUBG",
