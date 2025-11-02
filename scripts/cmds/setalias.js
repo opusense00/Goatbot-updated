cb83f89af8765dd483823e5bc37a944705bad5e0
opusense00 <opusense00@gmail.com>
2025-11-03 01:53:03 +0600

    Update setalias.js

--- scripts/cmds/setalias.js
+++ scripts/cmds/setalias.js
@@ -9,7 +9,7 @@ module.exports = {
 			vi: "Thêm tên gọi khác cho 1 lệnh bất kỳ trong nhóm của bạn",
 			en: "Add an alias for any command in your group"
 		},
-		category: "config",
+		category: "box chat",
 		guide: {
 			vi: "  Lệnh dùng để thêm/xóa tên gọi khác cho 1 lệnh nào đó để tiện sử dụng trong nhóm chat của bạn"
 				+ "\n   {pn} add <tên gọi khác> <tên lệnh>: dùng để thêm tên gọi khác cho lệnh trong nhóm chat của bạn"
